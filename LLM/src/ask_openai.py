167a83d4a0d275e5c34e3ce1a7fea7fb5005649e
yangyifanYYF <156071586+yangyifanYYF@users.noreply.github.com>
2024-11-16 19:00:04 +0800

    Update ask_openai.py

--- LLM/src/ask_openai.py
+++ LLM/src/ask_openai.py
@@ -1,17 +1,16 @@
 from openai import OpenAI
 
-
 def ask(role, prompt):
-    api_key = "your key"
-    api_base = "https://pro.aiskt.com/v1"
+    api_key = "your key" # 初始化API密钥
+    api_base = "https://pro.aiskt.com/v1" # 中转网站网址
     client = OpenAI(api_key=api_key, base_url=api_base)
 
     completion = client.chat.completions.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": role},
-            {"role": "user", "content": prompt}
+            {"role": "system", "content": role}, # 角色扮演词
+            {"role": "user", "content": prompt} # 提示词
         ]
     )
 
-    return completion.choices[0].message
+    return completion.choices[0].message # 返回响应信息
